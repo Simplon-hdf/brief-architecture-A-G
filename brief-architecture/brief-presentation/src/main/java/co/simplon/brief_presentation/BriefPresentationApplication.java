4ad837e5970cd9f79d4c40d678d8d98f5465354e
Gabriel Luthun <gabriel.luthun@gmail.com>
2025-01-29 15:04:15 +0100

    docs(brief-presentation): add presentation structure

---
+++ brief-architecture/brief-presentation/src/main/java/co/simplon/brief_presentation/BriefPresentationApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.brief_presentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BriefPresentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BriefPresentationApplication.class, args);
+	}
+
+}
