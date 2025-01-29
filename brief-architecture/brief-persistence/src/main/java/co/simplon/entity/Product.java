35465119575790eb873bde4726b3f727731c41fc
Gabriel Luthun <gabriel.luthun@gmail.com>
2025-01-29 16:34:23 +0100

    fix(product.java): add product entity file

---
+++ brief-architecture/brief-persistence/src/main/java/co/simplon/entity/Product.java
@@ -0,0 +1,80 @@
+package co.simplon.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "product_id")
+    private Long productId;
+
+    @Column(name = "product_name", length = 50, nullable = false)
+    private String productName;
+
+    @Column(name = "product_description", length = 255, nullable = false)
+    private String productDescription;
+
+    @Column(name = "product_price", length = 15, nullable = false)
+    private double productPrice;
+    
+    // Constructeur sans argument requis par JPA
+    public Product() {
+    }
+
+    // Constructeur avec arguments
+    public Product(String productName, String productDescription, double productPrice) {
+        this.productName = productName;
+        this.productDescription = productDescription;
+        this.productPrice = productPrice;
+    }
+
+    // Getters et Setters
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", productName='" + productName + '\'' +
+                ", productDescription='" + productDescription + '\'' +
+                ", productPrice=" + productPrice +
+                '}';
+    }
+}
